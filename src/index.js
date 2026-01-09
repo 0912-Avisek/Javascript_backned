8289a4bb96da674169dcf501528700fa68f24784
0912-Avisek <abhikrq@gmail.com>
2026-01-09 10:47:50 +0530

    add some utils and use some middleware

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import dotenv from 'dotenv';
-import express from 'express';
 import connectDB from './db/dbConnect.js';
+import app from './app.js';
 
 dotenv.config({
     path: './.env'
 });
 
-connectDB();
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT, () =>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((error) => {
+    console.log("Failed to connect to DB", error);
+});
---
+++ utils/ApiResponse.js
@@ -0,0 +1,7 @@
+class ApiResponse{
+    constructor(statusCode, message="success", data) {
+        this.statusCode = statusCode < 400;
+        this.message = message;
+        this.data = data;
+    }
+}

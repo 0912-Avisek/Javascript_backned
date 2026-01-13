67d8db3772fe4f4f115bb30e1c4fd76027a875b8
0912-Avisek <abhikrq@gmail.com>
2026-01-13 13:07:22 +0530

    added the cloudinary and multer

---
+++ utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+
+// Configuration
+cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME , 
+        api_key: process.env.CLOUDINARY_API_KEY , 
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
+
+
+export const uploadToCloudinary= async (localFilePath)=>{
+    try {
+        if(!localFilePath) return null;
+        
+        const data= await cloudinary.uploader.upload(localFilePath,{
+            resource_type: "auto"});
+
+         return data;
+
+    }catch (error) {
+        fs.unlinkSync(localFilePath);
+        console.error("Error uploading to Cloudinary:", error);
+        return null;
+    }
+
+}
+
